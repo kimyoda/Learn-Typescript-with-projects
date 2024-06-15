930d927b20ad4db0216eddffa1d8c5a9595d2487
kimyoda <rladygks1210@naver.com>
2024-06-15 15:22:13 +0900

    [feat] Typescript type advanced

---
+++ class-note/2_type-advanced.ts
@@ -0,0 +1,31 @@
+// 이넘(Enum)
+enum Avengers {
+  Capt,
+  Iroman,
+  Hulk,
+}
+const myHero = Avengers.Capt;
+
+// any
+let a: any = "hi";
+a = 20;
+a = false;
+
+// void
+function sayHi(): void {
+  // return "a";
+}
+
+// 타입 추정(type assertion)
+{
+  let str = "이건 문자열 타입이다.";
+}
+{
+  let num: number;
+  num = "str" as any;
+}
+
+const divElement = document.querySelector("#app");
+divElement?.innerHTML;
+// const emepty = document.querySelector("#app") as null;
+// empty.innerHTML;
